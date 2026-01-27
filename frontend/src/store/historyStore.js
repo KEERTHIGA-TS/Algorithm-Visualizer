1e34cd50e77a45205f339144e3b46e991b57829a
KEERTHIGA-TS <23cseb16keerthigats@gmail.com>
2026-01-27 20:41:24 +0530

    Fix history & progress API routes

--- frontend/src/store/historyStore.js
+++ frontend/src/store/historyStore.js
@@ -5,7 +5,7 @@ import { create } from "zustand";
 import axios from "axios";
 import { useAuthStore } from "./authStore";
 
-const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/auth`;
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/history`;
 
 export const useHistoryStore = create((set, get) => ({
   recentHistory: [],
